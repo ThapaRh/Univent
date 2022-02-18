11c5ff01311b8849a89405dcb49be9bcfc72955c
BenjaminStan <benjamin.stanelle@mavs.uta.edu>
2022-02-18 01:16:48 -0600

    s2

---
+++ react-refresher-09-two-way-binding/src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./index.css";
+import App from "./App";
+
+//renders the app in the place of the div "root"
+ReactDOM.render(<App />, document.getElementById("root"));
